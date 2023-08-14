7da9bfba1aad67724b0f7fab08312e2152bb3be7
lplnpfdfd <lplnpfdfd@gmail.com>
2023-08-14 10:16:31 +0330

    Repositori

---
+++ DomailLayer/DomainServices/Repositories/Contracts/IProductCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using DomailLayer.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomailLayer.DomainServices.Repositories.Contracts
+{
+    internal interface IProductCategoryRepository
+    {
+        public List<ProductCategory> GetAll();
+        public ProductCategory GetById(int id);
+        public void Remove(int id);
+        public void Remove(ProductCategory category);
+        public void Add(ProductCategory category);  
+        public void Update(ProductCategory category);
+    }
+}
