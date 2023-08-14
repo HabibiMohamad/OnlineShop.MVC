6968015c231d7ba01345b71045ae2f6044eb7dd8
lplnpfdfd <lplnpfdfd@gmail.com>
2023-08-14 11:53:43 +0330

    Contract

---
+++ ApplicationLayer/UseCases/Implementations/ProductCategoryUseCase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.UseCases.Implementations
+{
+    internal class ProductCategoryUseCase
+    {
+    }
+}
