07de34f37ac0d71664851cacb9e60559c6867b51
MohamadHabibi <lplnpfdfd@gmail.com>
2023-08-18 00:34:21 +0330

    Product

--- ApplicationLayer/UseCases/Contracts/IProductUseCase.cs
+++ ApplicationLayer/UseCases/Contracts/IProductUseCase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationLayer.Dtos.ProductDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace ApplicationLayer.UseCases.Contracts
 {
     public interface IProductUseCase
     {
-
+        void Create(Create_ProductDto dto);
+        Detail_ProductDetailDto Detail(int id);
+        List<GetAllForGrid_ProductDto> GetAllForGrid();
+        void Update(Uppdate_ProductDto dto);
     }
 }
